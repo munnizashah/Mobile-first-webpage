149d21444167cfa1289303881a1f81da86ddbcc6
Munniza <you@example.com>
2022-10-05 15:28:03 +0200

    first commit

---
+++ friday.js
@@ -0,0 +1,6 @@
+document.getElementById("finish-button").addEventListener("click", myFunction);
+
+function myFunction() {
+    document.getElementById("enjoy").innerHTML = "I hope you enjoyed my presentation!"
+
+}
